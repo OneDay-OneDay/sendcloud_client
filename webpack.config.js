03409aa841ff8ee0a08baf155b840809b6f0bb60
JerelLin <ohlinzepeng@sina.com>
2016-08-14 12:51:17 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,35 +7,35 @@ module.exports={
 	},
 	output: {
 		path: __dirname + '/public/out/',
-	    filename: "[name].js",
-	    publicPath: "/out/",
-	    chunkFilename: "[name].js"
+	    	filename: "[name].js",
+	    	publicPath: "/out/",
+	    	chunkFilename: "[name].js"
 	},
 	module: {
 	    loaders: [	
-			{ test: /\.js$/, loader : "babel", query: { presets: ["es2015"] } },
-			{ test: /\.css$/, loader: "style!css" },
-			{ test: /\.(jpg|png|otf)$/, loader: "url?limit=8192" },
-			{ test: /\.scss$/, loader : "style!css!sass" },
-			{ test: /\.jsx$/, loader: "babel", query: { presets: ["react", "es2015"] } }
+		{ test: /\.js$/, loader : "babel", query: { presets: ["es2015"] } },
+		{ test: /\.css$/, loader: "style!css" },
+		{ test: /\.(jpg|png|otf)$/, loader: "url?limit=8192" },
+		{ test: /\.scss$/, loader : "style!css!sass" },
+		{ test: /\.jsx$/, loader: "babel", query: { presets: ["react", "es2015"] } }
 	    ]
     },
     resolve: {
         extensions: ["", ".js", ".jsx",".scss"]
     },
     plugins: [
-		new webpack.optimize.CommonsChunkPlugin({
-		    names: ['vendor', 'manifest']
-		}),
-		new webpack.DefinePlugin({
+	new webpack.optimize.CommonsChunkPlugin({
+		names: ['vendor', 'manifest']
+	}),
+	new webpack.DefinePlugin({
 	    	'process.env' : {
 	        	'NODE_ENV' : JSON.stringify('production')
 	      	}
-	    }),
-	    new webpack.optimize.UglifyJsPlugin({
+	}),
+	new webpack.optimize.UglifyJsPlugin({
       		compress : {
         		warnings: false
       		}
     	})
-	]
+    ]
 }
